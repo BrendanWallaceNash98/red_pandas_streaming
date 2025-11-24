c3ac0df1ec2307794110caf9cbec93358f02aa9c
BrendanWallaceNash98 <brendan.wallacenash@connect.qut.edu.au>
2025-11-24 21:44:41 +1000

    customer creation working, with full street name creater too

--- data_creation/data_creation.py
+++ data_creation/data_creation.py
@@ -14,31 +14,79 @@ class Customer:
         self.id = uuid.uuid4()
         self.created_time = datetime.now()
         self.full_name = faker.name()
-        self.salulation = None
-        self.first_name = None
-        self.last_name = None
-        self.full_address = faker.address()
-        self.street_number = None
-        self.street_name = None
-        self.postcode = None
-        self.state = None
-        self.country = None
+        self.salulation = self.get_salutation()
+        self.first_name = self.get_first_name()
+        self.last_name = self.get_last_name()
+        self.full_address = faker.address().replace("\n", " ")
+        self.street_number = self.get_address_number()
+        self.street_name = self.get_street_name()
+        self.city = self.get_city()
+        self.postcode = self.get_postcode()
+        self.state = self.get_state()
+
+    def get_salutation(self) -> str:
+        if self.full_name is None:
+            return ""
+        name_parts = self.full_name.split(" ")
+        if name_parts[0].__contains__("."):
+            return name_parts[0]
+        return ""
+
+    def get_first_name(self) -> str:
+        if self.full_name is None:
+            return ""
+        name_parts = self.full_name.split(" ")
+        if name_parts[0].__contains__("."):
+            return name_parts[1]
+        return name_parts[0]
+
+    def get_last_name(self) -> str:
+        if self.full_name is None:
+            return ""
+        name_parts = self.full_name.split(" ")
+        if name_parts[0].__contains__("."):
+            return name_parts[2]
+        return name_parts[1]
+
+    def get_address_number(self) -> int:
+        try:
+            num = int(self.full_address.split(" ")[0])
+            return num
+        except Exception:
+            return 9999999999
+
+    def get_street_name(self) -> str:
+        addy = self.full_address.split(",")
+        return " ".join(addy[0].split(" ")[1:-1])
+
+    def get_city(self) -> str:
+        addy = self.full_address.split(",")
+        return addy[0].split(" ")[-1]
+
+    def get_postcode(self) -> str:
+        try:
+            addy = self.full_address.split(",")
+            return addy[1].split()[-1]
+        except Exception as e:
+            return ""
+
+    def get_state(self) -> str:
+        try:
+            addy = self.full_address.split(",")
+            return addy[1].split()[0]
+        except Exception as e:
+            return ""
 
 
 class Orders:
-    def __init__(self, new_customer: bool, transaction: bool):
-        self.new_customer = new_customer
-        self.new_transaction = new_transaction
-        self
+    def __init__(self, customer: Customer):
+        self.created_at = datetime.now()
+        self.id = uuid.uuid4()
+        self.customer_id = customer.id
+        self.order_products = None
+        self.order_quantity = None
 
 
-def create_transactions():
-    return
-
-
-def create_user():
-    return
-
-
-def create_product():
-    return
+if __name__ == "__main__":
+    for i in range(100000):
+        print(Customer().__dict__)
